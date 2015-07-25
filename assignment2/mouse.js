68c3628c5bd0ddaafe1e99799b62f72ba39f48af
Eric Schwabe <eschwabe@gmail.com>
2015-07-24 22:51:30 -0700

    Implement drawing with mouse support and initial controls.

--- assignment2/mouse.js
+++ assignment2/mouse.js
@@ -2,71 +2,41 @@
 
 var canvas;
 var gl;
-
-var maxNumTriangles = 200;
-var maxNumVertices = 3 * maxNumTriangles;
-var index = 0;
-
+var colors = [];
+var offsets = [];
+var points = [];
+var points_size = 0;
 var redraw = false;
 
-var colors = [
+var color_table = [
   vec4(0.0, 0.0, 0.0, 1.0), // black
   vec4(1.0, 0.0, 0.0, 1.0), // red
   vec4(1.0, 1.0, 0.0, 1.0), // yellow
   vec4(0.0, 1.0, 0.0, 1.0), // green
   vec4(0.0, 0.0, 1.0, 1.0), // blue
   vec4(1.0, 0.0, 1.0, 1.0), // magenta
-  vec4(0.0, 1.0, 1.0, 1.0) // cyan
+  vec4(0.0, 1.0, 1.0, 1.0)  // cyan
 ];
 
-
+// Initialize window
 window.onload = function init() {
+
   canvas = document.getElementById("gl-canvas");
 
   gl = WebGLUtils.setupWebGL(canvas);
   if (!gl) {
-    alert("WebGL isn't available");
+    alert("WebGL is not available");
   }
 
-  canvas.addEventListener("mousedown", function(event) {
-    redraw = true;
-  });
-
-  canvas.addEventListener("mouseup", function(event) {
-    redraw = false;
-  });
-  //canvas.addEventListener("mousedown", function(){
-  canvas.addEventListener("mousemove", function(event) {
-
-    if (redraw) {
-      gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-      var t = vec2(2 * event.clientX / canvas.width - 1,
-        2 * (canvas.height - event.clientY) / canvas.height - 1);
-      gl.bufferSubData(gl.ARRAY_BUFFER, 8 * index, flatten(t));
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-      t = vec4(colors[(index) % 7]);
-      gl.bufferSubData(gl.ARRAY_BUFFER, 16 * index, flatten(t));
-      index++;
-    }
-
-  });
-
-
   gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(0.5, 0.5, 0.5, 1.0);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-
-  //
   //  Load shaders and initialize attribute buffers
-  //
   var program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
 
-
   var vBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, 8 * maxNumVertices, gl.STATIC_DRAW);
 
   var vPosition = gl.getAttribLocation(program, "vPosition");
   gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
@@ -74,22 +44,65 @@ window.onload = function init() {
 
   var cBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW);
 
   var vColor = gl.getAttribLocation(program, "vColor");
   gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(vColor);
 
+  gl.lineWidth(3);
+
   render();
+
+  // Handle mousedown event
+  canvas.addEventListener("mousedown", function(event) {
+    redraw = true;
+
+    // Start new line segment
+    offsets.push(Math.max(0, points.length-1));
+    points.push(computePoint(event, canvas));
+    colors.push(vec4(color_table[0]));
+  });
+
+  // Handle mouseup event
+  canvas.addEventListener("mouseup", function(event) {
+    redraw = false;
+    points.push(computePoint(event, canvas));
+    colors.push(vec4(color_table[0]));
+  });
+
+  // Handle mousemove event
+  canvas.addEventListener("mousemove", function(event) {
+    if (redraw) {
+      // Compute and store new vertex
+      points.push(computePoint(event, canvas));
+      points.push(computePoint(event, canvas));
+      gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+      points_size = points.length-1;
+
+      // Store vertex color
+      gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+      colors.push(vec4(color_table[0]));
+      colors.push(vec4(color_table[0]));
+      gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    }
+  });
 
 }
 
+// Compute mouse pointer location from event
+function computePoint(event, canvas) {
+  // var vertex = vec2(2 * event.clientX / canvas.width - 1,
+  //   2 * (canvas.height - event.clientY) / canvas.height - 1);
+  var rect = event.target.getBoundingClientRect();
+  var x = ((event.clientX - rect.left) - canvas.width/2)/(canvas.width/2);
+  var y = (canvas.height/2 - (event.clientY - rect.top))/(canvas.height/2);
+  return vec2(x,y);
+}
 
+// Render
 function render() {
-
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, index);
-
+  gl.drawArrays(gl.LINES, 0, points_size);
   window.requestAnimFrame(render);
-
 }
